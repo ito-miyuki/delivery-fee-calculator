2552533c1729d0fb5046fa5dc6c63eda53b10e60
Miyuki Ito <miyuki.ito@slurp.coffee>
2025-01-10 23:06:57 +0200

    finished simple version of stylying

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -27,60 +27,68 @@ function Form() {
             <form id="form" onSubmit={handleSubmit}>
                 <div className="form-group">
                     <label htmlFor="cartValue" data-testid="cart-value-label">Cart value</label>
-                    <input
-                        type="number"
-                        id="cartValue"
-                        name="cartValue"
-                        placeholder="0.0"
-                        min="0.1"
-                        step="0.1"
-                        value={cartValue}
-                        onChange={(e) => setCartValue(parseFloat(e.target.value))}
-                        // why parse? because value in input is a string even tyoe is number.
-                    />
-                    <span>€</span>
+                    <div className="input-wrapper">
+                        <input
+                            type="number"
+                            id="cartValue"
+                            name="cartValue"
+                            placeholder="0.0"
+                            min="0.1"
+                            step="0.1"
+                            value={cartValue}
+                            onChange={(e) => setCartValue(parseFloat(e.target.value))}
+                            // why parse? because value in input is a string even tyoe is number.
+                        />
+                        <span>€</span>
+                    </div>
                 </div>
 
                 <div className="form-group">
                     <label htmlFor="deliveryDistance">Delivery distance</label>
-                    <input
-                        type="number"
-                        id="deliveryDistance"
-                        name="deliveryDistance"
-                        placeholder="0"
-                        min="0"
-                        step="1"
-                        value={deliveryDistance}
-                        onChange={(e) => setDeliveryDistance(parseFloat(e.target.value))}
-                    />
-                    <span>m</span>
+                    <div className="input-wrapper">
+                        <input
+                            type="number"
+                            id="deliveryDistance"
+                            name="deliveryDistance"
+                            placeholder="0"
+                            min="0"
+                            step="1"
+                            value={deliveryDistance}
+                            onChange={(e) => setDeliveryDistance(parseFloat(e.target.value))}
+                        />
+                        <span>m</span>
+                    </div>
                 </div>
 
                 <div className="form-group">
                     <label htmlFor="numberOfItems">Amount of items</label>
-                    <input
-                        type="number"
-                        id="numberOfItems"
-                        name="numberOfItems"
-                        placeholder="0"
-                        min="0"
-                        step="1"
-                        value={numberOfItems}
-                        onChange={(e) => setNumberOfItems(parseFloat(e.target.value))}
-                    />
+                    <div className="input-wrapper">
+                        <input
+                            type="number"
+                            id="numberOfItems"
+                            name="numberOfItems"
+                            placeholder="0"
+                            min="0"
+                            step="1"
+                            value={numberOfItems}
+                            onChange={(e) => setNumberOfItems(parseFloat(e.target.value))}
+                        />
+                    </div>
                 </div>
 
                 <div className="form-group">
                     <label htmlFor="orderTime">Time</label>
-                    <input
-                        type="datetime-local"
-                        id="orderTime"
-                        name="orderTime"
-                        placeholder="Select Order Time"
-                        min={now.toISOString().slice(0, 16)}
-                        value={orderTime}
-                        onChange={(e) => setOrderTime(e.target.value)}
-                    />
+                    <div className="input-wrapper">
+                        <input
+                            type="datetime-local"
+                            id="orderTime"
+                            name="orderTime"
+                            placeholder="Select Order Time"
+                            min={now.toISOString().slice(0, 16)}
+                            value={orderTime}
+                            onChange={(e) => setOrderTime(e.target.value)}
+                        />
+                    </div>
                 </div>
 
             <button type="submit">Calculate delivery price</button>
